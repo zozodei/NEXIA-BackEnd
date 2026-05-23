aa7842972b955c4ad0c32a38d8416ca292313fd8
zozodei <zodoze8@gmail.com>
2026-05-23 13:26:55 -0300

    asd

---
+++ src/database/db.js
@@ -0,0 +1,19 @@
+import pg from 'pg';
+import dotenv from 'dotenv';
+
+dotenv.config();  
+const { Pool } = pg;
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+});
+console.log(process.env.DATABASE_URL);
+pool.connect((err, client, release) => {
+  if (err) {
+    return console.error('Error adquiriendo el cliente', err.stack);
+  }
+  console.log('Conexión exitosa a Supabase');
+  release();
+});
+
+export default pool;
